f755f86cca9384f0227c1bf811badbde77734af6
HoA-W <932552424@qq.com>
2021-04-23 18:23:27 +0800

    fix

--- utils/util.go
+++ utils/util.go
@@ -2,20 +2,23 @@ package utils
 
 import (
 	"course/config"
-	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 // 读取配置文件
 func UnmarshalConf(conf *config.TotalConf) error {
 	env, ok := os.LookupEnv("CUR_ENV")
-	if !ok {
-		return fmt.Errorf("Not Found ENV: CUR_ENV")
-	}
 	var confByt []byte
 	var err error
+	if !ok {
+		confByt, err = ioutil.ReadFile("./conf/myConf_pro.yml")
+		//return fmt.Errorf("Not Found ENV: CUR_ENV")
+		log.Println("Not Found ENV: CUR_ENV")
+		return nil
+	}
 	if env == "test" {
 		confByt, err = ioutil.ReadFile("./conf/myConf_test.yml")
 	} else {
