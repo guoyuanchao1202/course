32cac6fb821309241077c29e0065ef2ed9993cbd
HoA-W <932552424@qq.com>
2021-04-23 18:29:32 +0800

    add log

--- dal/init.go
+++ dal/init.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"log"
 )
 
 var Dao *gorm.DB
@@ -12,6 +13,7 @@ var Dao *gorm.DB
 // 初始化MySQL
 func InitMySQL(mysqlConf config.MySQLConfig) (err error) {
 	format := "%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local"
+	log.Println(format)
 	// 用户名-密码-主机名-端口号-数据库名
 	DSN := fmt.Sprintf(format, mysqlConf.DBUser, mysqlConf.DBPassWord, mysqlConf.DBHost, mysqlConf.DBPort, mysqlConf.DBName)
 	Dao, err = gorm.Open("mysql", DSN)
--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-#!bin/bash
+#!bin/sh
 export GIN_MODE=release
+echo "start run.sh"
+cd /root/go/src/course
 ./main
+if [ $? -ne 0 ]; then
+    echo "failed"
+else
+    echo "succeed"
+fi
